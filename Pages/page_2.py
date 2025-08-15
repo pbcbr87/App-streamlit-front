fb7a7d6adab3ffd1bf0fa46a56df2a07f9df05a7
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-08-15 12:26:02 -0300

    Ajustes no grafico

--- Pages/page_2.py
+++ Pages/page_2.py
@@ -59,7 +59,7 @@ with tab2:
             title=dict(text="Valor em Reais"),
             side="left",
             range=None,
-            tickformat=".00f"
+            tickformat=",.00f"
         ),
         yaxis2=dict(
             title=dict(text="Lucro %"),
