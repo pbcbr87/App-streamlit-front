785923213c43089b88dde5ad424c3d9910529501
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-08-19 19:50:42 -0300

    Organizar paginas

--- app_st.py
+++ app_st.py
@@ -71,7 +71,7 @@ def home():
 if st.session_state.logado == False:
     pg = st.navigation([st.Page(login), st.Page('Pages/page_bruno.py', title='Bruno')])
 else:
-    pg = st.navigation([st.Page(home,title='inicio'), st.Page(f'Pages/page_bruno.py', title='Teste'), st.Page('Pages/page_1.py', title='Operações'), st.Page('Pages/page_2.py', title='Carteira')])            
+    pg = st.navigation([st.Page(home,title='inicio'), st.Page(f'Pages/page_empty.py', title='Teste'), st.Page('Pages/page_1.py', title='Operações'), st.Page('Pages/page_2.py', title='Carteira')])            
 
 
 pg.run()
