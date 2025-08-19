26b80a7f25ea76d2169b0033f9a375a95e230894
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-08-19 19:57:18 -0300

    organizar paginas

--- Pages/page_empty.py
+++ Pages/page_empty.py
@@ -1,11 +1,4 @@
 import streamlit as st
 
 
-pagina = st.empty()
-
-if st.button('pagina'):
-    pagina.markdown("Poof!")
-    st.switch_page('Pages/page_bruno.py')
-
-if st.button('pagina2'):
-    pagina.markdown("opa!")
+st.text('Ola')
