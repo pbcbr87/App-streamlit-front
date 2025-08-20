df8e9177d797cab2157460038923d07b3f4d82cc
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-08-20 12:40:47 -0300

    grafico pizza

--- Pages/page_2.py
+++ Pages/page_2.py
@@ -111,7 +111,7 @@ with tab2:
     st.plotly_chart(fig, use_container_width=True)
 
 with tab3:
-    df = df_carteira.groupby('categoria')
+    df = df_carteira.groupby('categoria')['valor_mercado_brl'].sum()
     st.dataframe(df)
 
 
