be453cb3bab5e31c1e091c16d550553d6646d81e
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-08-20 18:43:13 -0300

    testes Grafico pizza

--- Pages/page_2.py
+++ Pages/page_2.py
@@ -111,9 +111,21 @@ with tab2:
     st.plotly_chart(fig, use_container_width=True)
 
 with tab3:
-    # df = df_carteira
-    # fig = go.Figure(data=[go.Pie(labels=df['categoria'], values=df['valor_mercado_brl'])])
-    # fig.show()
+    import pandas as pd
+
+    df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Contestant": ["Alex", "Alex", "Alex", "Jordan", "Jordan", "Jordan"],
+    "Number Eaten": [2, 1, 3, 1, 3, 2],
+    })
+
+
+    # Plotly Express
+
+    import plotly.express as px
+
+    fig = px.bar(df, x="Fruit", y="Number Eaten", color="Contestant", barmode="group")
+    fig.show()
 
 
 
