1d39836fd0d44b55f5804b3b75c7aedd05918d99
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-08-20 18:57:38 -0300

    pizza

--- Pages/page_2.py
+++ Pages/page_2.py
@@ -125,7 +125,7 @@ with tab3:
     import plotly.express as px
     
     fig = px.pie(df, values='valor_mercado_brl', names='categoria', title='Tipo de ativos',
-             hover_data=['categoria'], labels={'categoria':'categoria 2')
+             hover_data=['categoria'], labels={'categoria':'categoria 2'})
     st.plotly_chart(fig, use_container_width=False)
 
     # import plotly.graph_objects as go
