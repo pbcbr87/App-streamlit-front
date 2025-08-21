37ef76e14dfed858c4c70037cde61713d53ce13e
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-08-21 18:41:56 -0300

    layout

--- Pages/page_2.py
+++ Pages/page_2.py
@@ -43,8 +43,7 @@ with col2:
 mask = df_carteira['categoria'].isin(Categoria)
 df_carteira = df_carteira[mask]
 
-# Criar abas
-tab1, tab2, tab3 = st.tabs(["Carteira", "Grafico barra", "Grafico pizza"])
+
 
 # ordenação
 df_carteira = df_carteira.sort_values('valor_mercado_brl', ascending=[False])
@@ -59,104 +58,103 @@ with st.container(horizontal=True, horizontal_alignment='left'):
     st.metric(label="Valor de mercado", value=f'{valor_total} R$')
     st.metric(label="Lucro", value=lucro_total, delta=f'{lucro_total_perc} %')
 
-st.text('test')
 
-with st.container(border=True):
-    st.text('teste')
+# Criar abas
+container_2 = st.container(border=True)
+tab1, tab2, tab3 = container_2.tabs(["Carteira", "Grafico barra", "Grafico pizza"])
+with tab1:  
+    df_carteira_st = (df_carteira.style.format(precision=2, thousands=".", decimal=",", subset=['quant',
+                                                                                                'custo_brl',
+                                                                                                'custo_usd',
+                                                                                                'valor_mercado_brl',
+                                                                                                'valor_mercado_usd',
+                                                                                                'lucro_brl',
+                                                                                                'lucro_usd',
+                                                                                                'valor_plan_brl',
+                                                                                                'valor_plan_usd'])
+                                .format(precision=0, thousands=".", decimal=",", subset=['peso', 'nota'])
+                                .format(precision=2, thousands=".", decimal=",", subset=['%']))
+
+    st.dataframe(df_carteira_st, hide_index=True, 
+                    column_config={
+                        "%": st.column_config.NumberColumn(
+                            "% (BRL)",
+                            help="Proporcação do ativo na carteira em BRL"
+                            )
+                        })
+
+with tab2:
+    df = df_carteira
+    fig = go.Figure()
+    fig.update_layout()
+
+    y = df['custo_brl']
+    x = df['codigo_ativo']
+    fig.add_trace(go.Bar(x=x.values, y=y.values,name='Custo'))
+
+    y = df['valor_mercado_brl']
+    fig.add_trace(go.Bar(x=x.values, y=y.values, name='Valor Atual'))
+
+    y = df['valor_plan_brl']
+    fig.add_trace(go.Bar(x=x.values, 
+                        y=y.values, 
+                        name='Valor Planejado',
+                        marker=dict(color='green', line=dict(color='black'))
+                        ))
+
+    y = df['%_lucro']
+    fig.add_trace(go.Bar(x=x.values, 
+                        y=y.values, 
+                        yaxis="y2", 
+                        name='Lucro %', 
+                        textposition='auto',
+                        texttemplate = "%{value:.2%}",
+                        textfont = dict(color='black'),
+                        marker=dict(color=y.where(y > 0 , 'IndianRed').where(y <= 0 , 'gray'),
+                                    opacity=0.5,
+                                    line=dict(color='black',width=1))))
+    fig.update_layout(
+        separators= ",.",
+        yaxis=dict(
+            title=dict(text="Valor em Reais"),
+            side="left",
+            range=None,
+            tickformat=",.2f"   
+        ),
+        yaxis2=dict(
+            title=dict(text="Lucro %"),
+            side="right",
+            range=None,
+            overlaying="y",
+            tickmode="sync",
+            tickformat=".2%"  
+        ),
+        legend=dict(orientation='h', yanchor='top', y=-0.5,xanchor='center',x=0.5,bgcolor='rgba(0,0,0,0)')
+    )
+    st.plotly_chart(fig, use_container_width=True)
+
+with tab3:
+
+    df = df_carteira
+
+    # Plotly Express
+
+    import plotly.express as px
     
-    with tab1:  
-        df_carteira_st = (df_carteira.style.format(precision=2, thousands=".", decimal=",", subset=['quant',
-                                                                                                    'custo_brl',
-                                                                                                    'custo_usd',
-                                                                                                    'valor_mercado_brl',
-                                                                                                    'valor_mercado_usd',
-                                                                                                    'lucro_brl',
-                                                                                                    'lucro_usd',
-                                                                                                    'valor_plan_brl',
-                                                                                                    'valor_plan_usd'])
-                                    .format(precision=0, thousands=".", decimal=",", subset=['peso', 'nota'])
-                                    .format(precision=2, thousands=".", decimal=",", subset=['%']))
-
-        st.dataframe(df_carteira_st, hide_index=True, 
-                        column_config={
-                            "%": st.column_config.NumberColumn(
-                                "% (BRL)",
-                                help="Proporcação do ativo na carteira em BRL"
-                                )
-                            })
-
-    with tab2:
-        df = df_carteira
-        fig = go.Figure()
-        fig.update_layout()
-
-        y = df['custo_brl']
-        x = df['codigo_ativo']
-        fig.add_trace(go.Bar(x=x.values, y=y.values,name='Custo'))
-
-        y = df['valor_mercado_brl']
-        fig.add_trace(go.Bar(x=x.values, y=y.values, name='Valor Atual'))
-
-        y = df['valor_plan_brl']
-        fig.add_trace(go.Bar(x=x.values, 
-                            y=y.values, 
-                            name='Valor Planejado',
-                            marker=dict(color='green', line=dict(color='black'))
-                            ))
-
-        y = df['%_lucro']
-        fig.add_trace(go.Bar(x=x.values, 
-                            y=y.values, 
-                            yaxis="y2", 
-                            name='Lucro %', 
-                            textposition='auto',
-                            texttemplate = "%{value:.2%}",
-                            textfont = dict(color='black'),
-                            marker=dict(color=y.where(y > 0 , 'IndianRed').where(y <= 0 , 'gray'),
-                                        opacity=0.5,
-                                        line=dict(color='black',width=1))))
-        fig.update_layout(
-            separators= ",.",
-            yaxis=dict(
-                title=dict(text="Valor em Reais"),
-                side="left",
-                range=None,
-                tickformat=",.2f"   
-            ),
-            yaxis2=dict(
-                title=dict(text="Lucro %"),
-                side="right",
-                range=None,
-                overlaying="y",
-                tickmode="sync",
-                tickformat=".2%"  
-            ),
-            legend=dict(orientation='h', yanchor='top', y=-0.5,xanchor='center',x=0.5,bgcolor='rgba(0,0,0,0)')
-        )
-        st.plotly_chart(fig, use_container_width=True)
-
-    with tab3:
-
-        df = df_carteira
-
-        # Plotly Express
-
-        import plotly.express as px
-        
-        fig = px.pie(df, values='valor_mercado_brl', names='categoria', title='Tipo de ativos',
-                hover_data=['valor_mercado_usd'], labels={'valor_mercado_usd':'Valor Mercado em Dolar'})
-        fig.update_traces(textposition='inside', textinfo='percent+label')
-        fig.update_layout(title={'y':0.9, 'x':0.5, 'xanchor':'center', 'yanchor':'top'})
+    fig = px.pie(df, values='valor_mercado_brl', names='categoria', title='Tipo de ativos',
+            hover_data=['valor_mercado_usd'], labels={'valor_mercado_usd':'Valor Mercado em Dolar'})
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+    fig.update_layout(title={'y':0.9, 'x':0.5, 'xanchor':'center', 'yanchor':'top'})
 
 
-        fig2 = px.pie(df, values='valor_mercado_brl', names='codigo_ativo', title='Ativos',
-                hover_data=['valor_mercado_usd'], labels={'valor_mercado_usd':'Valor Mercado em Dolar'})
-        fig2.update_traces(textposition='inside', textinfo='percent+label')
-        fig2.update_layout(title={'y':0.9, 'x':0.5, 'xanchor':'center', 'yanchor':'top'})
+    fig2 = px.pie(df, values='valor_mercado_brl', names='codigo_ativo', title='Ativos',
+            hover_data=['valor_mercado_usd'], labels={'valor_mercado_usd':'Valor Mercado em Dolar'})
+    fig2.update_traces(textposition='inside', textinfo='percent+label')
+    fig2.update_layout(title={'y':0.9, 'x':0.5, 'xanchor':'center', 'yanchor':'top'})
 
-        with st.container(horizontal=True, horizontal_alignment='left'):
-            st.plotly_chart(fig)
-            st.plotly_chart(fig2)
+    with st.container(horizontal=True, horizontal_alignment='left'):
+        st.plotly_chart(fig)
+        st.plotly_chart(fig2)
 
 
 
