3c42f6faef6da0aeb22b33af4487457407904597
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-08-22 07:58:06 -0300

    layout

--- Pages/page_2.py
+++ Pages/page_2.py
@@ -41,7 +41,7 @@ if 'Key_SL_2' not in st.session_state:
 col1, col2 = sl_cat_container.columns([1, 0.1])
 with col1:
         Categoria = st.multiselect('categoria', df_cat, placeholder = f'Selecione quals categorias', key='Key_SL_2')
-with col2(medium):
+with col2(gap ='medium'):
     # st.write('')
     # st.text('')
     st.button(':material/checklist_rtl:', help='Selecionar tudo', key='Key_BT_2', on_click=sl_tudo_ex)
