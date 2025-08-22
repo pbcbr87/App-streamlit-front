4230d7683d21ed552d815fb0f0dc1a2c0b9b183f
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-08-22 07:13:07 -0300

    numero

--- Pages/page_2.py
+++ Pages/page_2.py
@@ -60,7 +60,7 @@ with metrica_total_container:
     lucro_total = round(valor_total - custo_total,2)
     lucro_total_perc = round(100*(valor_total - custo_total) / custo_total,2)
 
-    valor_total = f"{valor_total:,}"
+    valor_total = f"{valor_total:.,}"
 
 
     st.metric(label="Valor de mercado", value=f'{valor_total} R$')
