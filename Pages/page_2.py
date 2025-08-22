f0bffc70f868e7644514210220ae9e917ca82e3f
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-08-22 18:03:21 -0300

    layout

--- Pages/page_2.py
+++ Pages/page_2.py
@@ -46,7 +46,7 @@ if 'Key_SL_2' not in st.session_state:
 with sl_cat_container:
     mult_sl_cat = st.pills('categoria', df_cat, key='Key_SL_2', selection_mode="multi")
 # with col2:
-    with st.container(horizontal=True, horizontal_alignment='left'):
+    with st.container(horizontal=True, horizontal_alignment='left', border=True):
         st.button("",icon=':material/cancel:', type='tertiary', help='Desmarcar tudo', key='Key_BT_3', on_click=sl_nada_ex)
         st.button("",icon=':material/checklist_rtl:', type='tertiary', help='Selecionar tudo', key='Key_BT_2', on_click=sl_tudo_ex)
         ck_box_plan = st.checkbox('Aplicar no Planejamento', help='O filtro será aplicado para recalcular os valores de planejamento')
