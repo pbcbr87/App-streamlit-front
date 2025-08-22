d11f91c9a8f9fc58582823de36c9726079a67700
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-08-22 07:56:26 -0300

    laytout

--- Pages/page_2.py
+++ Pages/page_2.py
@@ -42,8 +42,8 @@ col1, col2 = sl_cat_container.columns([1, 0.1])
 with col1:
         Categoria = st.multiselect('categoria', df_cat, placeholder = f'Selecione quals categorias', key='Key_SL_2')
 with col2:
-    st.text('/n')
-    # st.text('')
+    st.write('')
+    st.text('')
     st.button(':material/checklist_rtl:', help='Selecionar tudo', key='Key_BT_2', on_click=sl_tudo_ex)
 
 mask = df_carteira['categoria'].isin(Categoria)
