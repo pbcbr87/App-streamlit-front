87f2d2c850872b93f0ac09c98fc05ec8e857eda3
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-08-22 08:11:28 -0300

    layout

--- Pages/page_2.py
+++ Pages/page_2.py
@@ -42,7 +42,7 @@ col1, col2 = sl_cat_container.columns([1, 0.1], border=True)
 with col1:
     Categoria = st.multiselect('categoria', df_cat, placeholder = f'Selecione quals categorias', key='Key_SL_2')
 with col2:
-    st.container(border=True, horizontal_alignment='center'):
+    with st.container(border=True, horizontal_alignment='center'):
         st.text('')
         st.text('')
         st.button(':material/checklist_rtl:', help='Selecionar tudo', key='Key_BT_2', on_click=sl_tudo_ex)
