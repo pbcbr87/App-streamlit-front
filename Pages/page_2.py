87ed30b322fb04ce492e0a2625dc4bdce0e1577a
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-08-22 23:55:56 -0300

    grafico

--- Pages/page_2.py
+++ Pages/page_2.py
@@ -192,8 +192,7 @@ with tab2:
             tickformat=".2%"  
         ),
         legend=dict(orientation='h', yanchor='top', y=-0.1,xanchor='center',x=0.5,bgcolor='rgba(0,0,0,0)'),
-        margin=dict(b=10,t=40),        
-        paper_bgcolor='theme.primaryColor' 
+        margin=dict(b=10,t=40) 
         )
     st.plotly_chart(fig, use_container_width=True)
 
