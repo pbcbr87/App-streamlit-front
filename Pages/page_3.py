1451e767022bae78e5b0c99587648269bef2529f
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-09-17 14:13:23 -0300

    planejamento

--- Pages/page_3.py
+++ Pages/page_3.py
@@ -15,37 +15,4 @@ if st.session_state['carteira_api'] == []:
 if not st.session_state['carteira_api'] == []:
     df_carteira = pd.DataFrame(st.session_state['carteira_api'])
     
-    df_carteira['pais'] = np.where((df_carteira['categoria'] == "AÇÕES") | (df_carteira['categoria'] == "FII"), 'BRL', 'USD')
-    df_carteira['%_lucro'] =  df_carteira['lucro_brl'] / df_carteira['custo_brl']
-
-    df_carteira_front = pd.DataFrame()
-    df_carteira_front['Código ativo'] = df_carteira['codigo_ativo']
-    df_carteira_front['Categoria'] = df_carteira['categoria']
-    df_carteira_front['País'] = df_carteira['pais']
-    df_carteira_front['Nome'] = df_carteira['nome']
-    df_carteira_front['Setor'] = df_carteira['setor']
-    df_carteira_front['Qt'] = df_carteira['quant']
-    df_carteira_front['Custo'] = df_carteira['custo_brl']
-    df_carteira_front['Valor de mercado'] = df_carteira['valor_mercado_brl']
-    df_carteira_front['Lucro'] = df_carteira['lucro_brl']
-    df_carteira_front['Lucro %'] = df_carteira['%_lucro']
-    df_carteira_front['Peso'] = df_carteira['peso']
-    df_carteira_front['Nota'] = df_carteira['nota']
-    df_carteira_front['Valor Planejado'] = df_carteira['valor_plan_brl']
-    df_carteira_front['Aporte'] = df_carteira_front['Valor Planejado'] - df_carteira_front['Valor de mercado']
-    df_carteira_front['Aporte %'] = df_carteira_front['Aporte']/df_carteira_front['Valor Planejado']
-    
-    df_carteira_st = (df_carteira_front.style.format(precision=2, thousands=".", decimal=",", subset=['Qt',
-                                                                                                    'Custo',
-                                                                                                    'Valor de mercado',
-                                                                                                    'Lucro',
-                                                                                                    'Valor Planejado',
-                                                                                                    'Aporte'])
-                                            .format(precision=0, thousands=".", decimal=",", subset=['Peso', 'Nota'])
-                                            .format(precision=2, thousands=".", decimal=",", subset=['Lucro %', 'Aporte %'])
-                                            )
-    st.dataframe(df_carteira_st, hide_index=True, use_container_width=True,
-                    column_config={
-                        "Lucro %": st.column_config.NumberColumn("Lucro %", format="percent"),
-                        "Aporte %": st.column_config.NumberColumn("Aporte %", format="percent")
-                        })
+    df_resp = st.data_editor(df_carteira, column_order=('codigo_ativo'))
