0eb5b3357b4dc8609f29ec5f6224afefc5925075
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-09-25 15:46:38 -0300

    adicionar peso

--- Pages/page_3.py
+++ Pages/page_3.py
@@ -27,6 +27,8 @@ def envia_manual(dados):
 #--------------------------------------------------
 # declaração de memoria
 #--------------------------------------------------
+if 'carteira_api' not in st.session_state:
+    st.session_state['carteira_api'] = False
 if st.session_state['carteira_api'] == False:
     #resp = requests.get(f'https://pythonapi-production-6268.up.railway.app/Calcular/calcular/{st.session_state.id}', headers={'Authorization':f'Bearer {st.session_state.token}'})
     st.session_state['carteira_api'] = requests.get(f'https://pythonapi-production-6268.up.railway.app/carteira/pegar_carteira', headers={'Authorization':f'Bearer {st.session_state.token}'}).json()
