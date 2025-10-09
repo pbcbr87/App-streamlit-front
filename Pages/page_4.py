defeec031d6d03011ef043dd825b83d205259367
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-10-09 16:34:14 -0300

    Update page_4.py

--- Pages/page_4.py
+++ Pages/page_4.py
@@ -8,3 +8,11 @@ from json import loads, dumps
 
 
 st.write("Aportes")
+
+#-----------------------------------------------------------
+# Buscando dados na API
+#-----------------------------------------------------------
+if st.session_state['carteira_api'] == False:
+    #resp = requests.get(f'https://pythonapi-production-6268.up.railway.app/Calcular/calcular/{st.session_state.id}', headers={'Authorization':f'Bearer {st.session_state.token}'})
+    st.session_state['carteira_api'] = requests.get(f'https://pythonapi-production-6268.up.railway.app/carteira/pegar_carteira', headers={'Authorization':f'Bearer {st.session_state.token}'}).json()
+
