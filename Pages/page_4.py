0e391ed91fd2dcc533153764c8fc81362c853b9c
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-10-09 11:53:06 -0300

    Update page_4.py

--- Pages/page_4.py
+++ Pages/page_4.py
@@ -1,1 +1,10 @@
 import streamlit as st
+import requests
+import pandas as pd
+import numpy as np
+from plotly import graph_objects as go
+import plotly.express as px
+from json import loads, dumps
+
+
+st.write("Aportes")
