ff1df463ae77d435aaaaf0b26704644669442b1c
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-10-09 16:45:36 -0300

    Update page_4.py

--- Pages/page_4.py
+++ Pages/page_4.py
@@ -12,6 +12,8 @@ st.write("Aportes")
 #-----------------------------------------------------------
 # Buscando dados na API
 #-----------------------------------------------------------
+if 'carteira_api' not in st.session_state:
+    st.session_state['carteira_api'] == False
 if st.session_state['carteira_api'] == False:
     #resp = requests.get(f'https://pythonapi-production-6268.up.railway.app/Calcular/calcular/{st.session_state.id}', headers={'Authorization':f'Bearer {st.session_state.token}'})
     st.session_state['carteira_api'] = requests.get(f'https://pythonapi-production-6268.up.railway.app/carteira/pegar_carteira', headers={'Authorization':f'Bearer {st.session_state.token}'}).json()
