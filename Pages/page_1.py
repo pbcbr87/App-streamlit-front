9ce60c1d54f770e0a13dd496a03368e57c0c8624
Patrick Brito Cangussu <patrickbc87@gmail.com>
2025-11-02 21:51:06 -0300

    Update page_1.py Op excluir

--- Pages/page_1.py
+++ Pages/page_1.py
@@ -46,7 +46,7 @@ def get_ativos():
 def excluir_op():
     try:
         lista_excluir = dumps(st.session_state['sl_op_excluir'])
-        resp = requests.delete(f'https://pythonapi-production-6268.up.railway.app/ordem_input/delete_ordem/, lista_excluir, headers={'Authorization':f'Bearer {st.session_state.token}'})
+        resp = requests.delete(f'https://pythonapi-production-6268.up.railway.app/ordem_input/delete_ordem/', lista_excluir, headers={'Authorization':f'Bearer {st.session_state.token}'})
         if resp.status_code == 200:
             st.toast('Dados Excluidos')
         else:
@@ -197,6 +197,7 @@ with tab4:
     else:
 
         st.write('Nenhum ordem cadastrada')
+
 
 
 
